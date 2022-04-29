f1b061b6eacdd329255a91a28f72906478adcf6a
Angelina <angelinarwhite@gmail.com>
2022-04-29 10:54:32 -0400

    commit tree bb40bc306f49a8992b8aeaa09d9f13df86180cf5

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+//remove main
+const main1 = document.getElementById("main");
+main1.remove();
+
+//add newHeader
+const newHeader = document.createElement("h1");
+newHeader.id = 'victory';
+newHeader.innerHTML = "YOUR-NAME is the champion"
+document.body.append(newHeader);
